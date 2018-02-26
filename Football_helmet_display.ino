0450436a806e3eca21c189e4e334da73d91d9f7a
Andy <andygrayia@gmail.com>
2018-02-26 14:18:42 -0500

    HELMET: ready for first demo

--- Football_helmet_display.ino
+++ Football_helmet_display.ino
@@ -2,32 +2,68 @@
 //   6       4
 //       7
 //   11       9
-//       10       8
+//      10       8
+
+// state 0 : helmet on bottom, no display
+// state 1 : helmet at top, button illuminated, no display
+// state 2 : helmet hits, display value for temporary time then return to state 0
+
 
 #include "SevSeg.h"
 SevSeg sevseg; //Instantiate a seven segment object
 
 int val = 0;
 float cm = 0;
- 
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(0, OUTPUT);  
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);  
-  pinMode(11, OUTPUT);  
-  
-  pinMode(22, OUTPUT); 
+float cm_old1 = 0;
+float cm_old2 = 0;
+float cm_old3 = 0;
+int demo = 0;
 
-  Serial.begin(9600);
+int state = 0;
+long clutch_timer = 0;
+int clutch_length = 3000;
+
+int button_state = 0;
+long button_timer = 0;
+bool blinker = 0;
+
+int break_beam = 0;
+long beam_timer = 0;
+
+int randNumber;
+
+long display_timer = 0;
+int display_length = 5000;
+
+long too_long_timer = 0;
+int too_long_length = 10000;
+bool sonar_state = 0;
+ 
+void setup() {      
+  randomSeed(analogRead(0));          
+  // initialize the digital pin as an output.
+  pinMode(0, OUTPUT); //right-most led display 
+  pinMode(1, OUTPUT); //led display
+  pinMode(2, OUTPUT); //led display
+  pinMode(3, OUTPUT); //left-most led display
+  pinMode(4, OUTPUT); //individual led
+  pinMode(5, OUTPUT); //individual led
+  pinMode(6, OUTPUT); //individual led
+  pinMode(7, OUTPUT); //individual led
+  pinMode(8, OUTPUT); //individual led
+  pinMode(9, OUTPUT); //individual led
+  pinMode(10, OUTPUT); //individual led 
+  pinMode(11, OUTPUT); //individual led
+
+  pinMode(12, OUTPUT); //Relay or clutch release 
+
+  pinMode(23, INPUT); //Button pressed
+  pinMode(22, OUTPUT); //Button LED
+
+  Serial.begin(115200);
+
+  digitalWrite(12, HIGH);
+  delay(1000);
 
   /*for (int i = 0; i < 50; i++){
     digitalWrite(22, HIGH);
@@ -55,135 +91,103 @@ void loop() {
   static unsigned long timer = millis();
   static int deciSeconds = 0;
 
+  cm_old3 = cm_old2;
+  cm_old2 = cm_old1;
+  cm_old1 = cm;
+
+  
   val = analogRead(7);     // read the input pin 
   val = val/2;
-  cm = val * 2.54;
-  //Serial.println(val);
-  if (cm <= 50){
-    digitalWrite(22,HIGH);
+  cm = ((val * 2.54) + cm_old1 + cm_old2 + cm_old3)/4.0;
+
+  button_state = digitalRead(23);
+
+  break_beam = analogRead(6);
+  //Serial.println(break_beam);
+   
+  if (break_beam >= 200 && state == 0){
+    too_long_timer = millis() + too_long_length;
+    state = 1;
+  }
+  else if (button_state == 0 && state == 1){
+    too_long_timer = millis() + too_long_length;
+    state = 2;
+    clutch_timer = millis() + clutch_length;
+  }
+  else if ((clutch_timer <= millis() || cm <= 50)&& state == 2){
+    too_long_timer = millis() + too_long_length;
+    randNumber = random(400, 3000);
+    sevseg.setNumber(randNumber,0);
+    display_timer = millis() + display_length;
+    state = 3;
+  }
+  else if (display_timer <= millis() && state == 3){
+    state = 0;
+  }
+  else if (too_long_timer <= millis() && state != 0 && state != 4){
+    state = 4;
+    clutch_timer = millis() + clutch_length;
+  }
+  else if ((clutch_timer <= millis() || cm <= 50)&& state == 4){
+    state = 0;
+  }
+ 
+  /*if (clutch_timer > millis()){
+    state = 1;
+  }
+  else if (button_timer > millis()){
+    state = 2;
   }
   else{
-    digitalWrite(22,LOW);
-  }
-  //delay(50);
+    state = 0;
+  }*/
   
-  if (millis() - timer >= 100) {
-    timer += 100;
+  if (millis() - timer >= 200) {
+    timer += 200;
     deciSeconds++; // 100 milliSeconds is equal to 1 deciSecond
-    
-    if (deciSeconds == 10000) { // Reset to 0 after counting for 1000 seconds.
-      deciSeconds=0;
+    if (state == 0){
+      digitalWrite(22, LOW);
+      digitalWrite(12, LOW);
+      //digitalWrite(22, LOW);
+      if (demo == 0){
+        sevseg.setChars("-   ");
+      }
+      else if (demo == 1){
+        sevseg.setChars(" -  ");
+      }
+      else if (demo == 2){
+        sevseg.setChars("  - ");
+      }      
+      else if (demo == 3){
+        sevseg.setChars("   -");
+      }      
+      demo++;
+      if (demo >= 4){
+        demo = 0;
+      }
     }
-    sevseg.setNumber(cm, 1);
+    else if (state == 1){
+       sevseg.blank();
+       digitalWrite(12, LOW);
+       digitalWrite(22, blinker);
+       blinker = !blinker;
+    }
+    else if (state == 2){
+       //sevseg.setChars("Andy");       
+       digitalWrite(22, HIGH);
+       digitalWrite(12, HIGH);
+    }  
+    else if (state == 3){
+       digitalWrite(22, LOW);
+       digitalWrite(12, LOW);
+    }
+    else if (state == 4){
+      sevseg.setChars("rest");
+      digitalWrite(12, HIGH);
+      digitalWrite(22, LOW);
+    }
   }
 
   sevseg.refreshDisplay(); // Must run repeatedly
-}
-
-void one(){
-  digitalWrite(13, LOW);
-  digitalWrite(14, LOW);
-  digitalWrite(15, HIGH);
-  digitalWrite(16, LOW);
-  digitalWrite(17, LOW);
-  digitalWrite(18, LOW);
-  digitalWrite(19, LOW);
-  digitalWrite(20, HIGH);
-}
-
-void two(){
-  digitalWrite(13, HIGH);
-  digitalWrite(14, HIGH);
-  digitalWrite(15, LOW);
-  digitalWrite(16, LOW);
-  digitalWrite(17, HIGH);
-  digitalWrite(18, LOW);
-  digitalWrite(19, HIGH);
-  digitalWrite(20, HIGH);
-}
-
-void three(){
-  digitalWrite(13, LOW);
-  digitalWrite(14, HIGH);
-  digitalWrite(15, HIGH);
-  digitalWrite(16, LOW);
-  digitalWrite(17, HIGH);
-  digitalWrite(18, LOW);
-  digitalWrite(19, HIGH);
-  digitalWrite(20, HIGH);
-}
-
-void four(){
-  digitalWrite(13, LOW);
-  digitalWrite(14, LOW);
-  digitalWrite(15, HIGH);
-  digitalWrite(16, LOW);
-  digitalWrite(17, HIGH);
-  digitalWrite(18, HIGH);
-  digitalWrite(19, LOW);
-  digitalWrite(20, HIGH);
-}
-
-void five(){
-  digitalWrite(13, LOW);
-  digitalWrite(14, HIGH);
-  digitalWrite(15, HIGH);
-  digitalWrite(16, LOW);
-  digitalWrite(17, HIGH);
-  digitalWrite(18, HIGH);
-  digitalWrite(19, HIGH);
-  digitalWrite(20, LOW);
-}
-
-void six(){
-  digitalWrite(13, HIGH);
-  digitalWrite(14, HIGH);
-  digitalWrite(15, HIGH);
-  digitalWrite(16, LOW);
-  digitalWrite(17, HIGH);
-  digitalWrite(18, HIGH);
-  digitalWrite(19, HIGH);
-  digitalWrite(20, LOW);
-}
-
-void seven(){
-  digitalWrite(13, LOW);
-  digitalWrite(14, LOW);
-  digitalWrite(15, HIGH);
-  digitalWrite(16, LOW);
-  digitalWrite(17, LOW);
-  digitalWrite(18, LOW);
-  digitalWrite(19, HIGH);
-  digitalWrite(20, HIGH);
-}
-
-void eight(){
-  digitalWrite(13, HIGH);
-  digitalWrite(14, HIGH);
-  digitalWrite(15, HIGH);
-  digitalWrite(16, LOW);
-  digitalWrite(17, HIGH);
-  digitalWrite(18, HIGH);
-  digitalWrite(19, HIGH);
-  digitalWrite(20, HIGH);
-}
-
-void nine(){
-  digitalWrite(13, LOW);
-  digitalWrite(14, HIGH);
-  digitalWrite(15, HIGH);
-  digitalWrite(16, LOW);
-  digitalWrite(17, HIGH);
-  digitalWrite(18, HIGH);
-  digitalWrite(19, HIGH);
-  digitalWrite(20, HIGH);
-}
-
-void decimal(){
-  digitalWrite(16, HIGH);
-}
-
-void no_decimal(){
-  digitalWrite(16, LOW);
 }
 
